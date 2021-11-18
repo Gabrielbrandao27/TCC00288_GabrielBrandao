b393619c2830b4c255dd26d2c9eb0a3ff5607ce4
Gabriel Brandão <gafbrandao@id.uff.br>
2021-11-18 20:17:02 -0300

    Exercicio3

---
+++ src/main/sql/s20212/exercicios/exercicio3/GabrielBrandao.sql
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  bielf
+ * Created: 18 de nov. de 2021
+ */
+
+CREATE FUNCTION MultMatriz() RETURNS float AS $$
+    BEGIN 
+    
+    END;
+$$ LANGUAGE plpgsql;
