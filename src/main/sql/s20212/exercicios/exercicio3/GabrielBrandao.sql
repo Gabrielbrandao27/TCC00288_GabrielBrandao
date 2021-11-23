c79a9612ca90e56051743e4070cc33db26d9f7b8
Gabriel Brandão <gafbrandao@id.uff.br>
2021-11-22 21:36:06 -0300

    Ex3

--- src/main/sql/s20212/exercicios/exercicio3/GabrielBrandao.sql
+++ src/main/sql/s20212/exercicios/exercicio3/GabrielBrandao.sql
@@ -18,33 +18,35 @@ END $$;
 
 CREATE TABLE matriz (
     mat1 float[][],
-    mat2 float[][],
-    mat3 float[][]
+    mat2 float[][]
 );
 
-insert into
+INSERT INTO matriz VALUES ('{{1,2,3}, {4,5,6}, {7,8,9}}', '{{1, 2, 3}}');
 
-CREATE FUNCTION multmat(mat1 float[][], mat2 float[][], OUT float[][]) AS $$
+CREATE FUNCTION multmat(mat1 float[][], mat2 float[][]) RETURNS float[][] AS $$
     
+    DECLARE 
+        mat3 float[][];
     BEGIN 
         for i in 1..array_length(mat1, 2) LOOP
-            for j in 1..array_length(mat1, 2) LOOP
-                for k in 1..aray_length(mat2, 1) LOOP
-                    for l in 1..array_length(mat2, 2) LOOP
-                        mat3[][]
+            for j in 1..array_length(mat2, 2) LOOP
+                for k in 1..array_length(mat2, 1) LOOP
+                    mat3[][] = mat1[i][k] * mat2[k][j];
+                    RETURN mat3[][];
+                END LOOP;
+            END LOOP;
+        END LOOP;
 
     EXCEPTION
         WHEN array_length(mat1, 2) != array_length(mat2, 1) 
         THEN 
             RAISE NOTICE 'Imcompativeis';
-            RETURN 0;
+            RETURN;
     END;
 
 $$ LANGUAGE plpgsql;
 
---SELECT * FROM MultMat(2, 5);
-
-DO $$ BEGIN    
-      raise notice 'Mensagem teste';
-      SELECT multmat();
+DO $$ BEGIN
+    SELECT multmat(SELECT mat1 FROM matriz, SELECT mat2 FROM matriz);
 END $$;
+
